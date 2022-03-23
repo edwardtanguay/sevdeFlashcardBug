74654fd941b222eb967df0d33dafdfa7fb205747
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-03-23 19:43:42 +0100

    clicking works

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// HTML ELEMENT VARIABLES
 const addButton = document.querySelector('#add-button')
 const frontText = document.querySelector('#front-word')
 const backText = document.querySelector('#back-word')
@@ -7,88 +8,59 @@ const buttonLeftElem = document.querySelector('.buttonLeft');
 const buttonRightElem = document.querySelector('.buttonRight');
 const clearBtn = document.querySelector('#clear')
 
+// GLOBAL VARIABLES
 let currentCardIndex = 0;
 const cards = []
 
-//document.addEventListener("DOMContentLoaded", loadAllCardsToUI);
-
-//direction buttons--------------------------------------------------------
+// RIGHT BUTTON
 buttonRightElem.addEventListener('click', function (e) {
-
 	currentCardIndex++;
-
 	if (currentCardIndex > cards.length - 1) {
 		currentCardIndex = cards.length - 1;
 	}
-
-	containerElem.children[currentCardIndex].classList.add('show')
-	containerElem.children[currentCardIndex - 1].classList.remove('show')
-
-	updateCurrentIndex();
-
+	displayCards();
 });
 
+// LEFT BUTTON
 buttonLeftElem.addEventListener('click', function (e) {
 	currentCardIndex--;
-
 	if (currentCardIndex < 1) {
 		currentCardIndex = 0;
 	}
-
-	containerElem.children[currentCardIndex + 1].classList.remove('show')
-	containerElem.children[currentCardIndex].classList.add('show')
-
-
-	updateCurrentIndex();
-
+	displayCards();
 });
 
-function updateCurrentIndex() {
-	currentEl.innerHTML = `${currentCardIndex + 1}/${cards.length} `
-}
-
 function displayCards(cards) {
-	const elem = document.createElement('div')
-	elem.className = 'inner-card'
+	containerElem.innerHTML = '<div class="card">';
 	cards.forEach((card) => {
-		elem.innerHTML = `
-        <div class="card-front ">
-          <p>
-            ${card.front}
-          </p>
-        </div>
-        <div class="card-back none">
-          <p>
-            ${card.back}
-          </p>
-        </div>
-        `;
-		containerElem.appendChild(elem)
-		containerElem.children[0].className = 'inner-card show'
-		if (cards.length > 1 && currentCardIndex > 0) {
-			containerElem.children[0].className = 'inner-card'
-		}
-		const frontCards = document.querySelectorAll('.card-front')
-		const backCards = document.querySelectorAll('.card-back')
-		const innerCard = document.getElementsByClassName('inner-card')
-
-		frontCards.forEach(frontCard => {
-			frontCard.addEventListener('click', function (e) {
-
-				frontCard.classList.add('none')
-				backCards[currentCardIndex].classList.remove('none')
-
-			});
-		});
-
-		backCards.forEach(backCard => {
-			backCard.addEventListener('click', function (e) {
-				backCard.classList.add('none')
-				frontCards[currentCardIndex].classList.remove('none')
-			});
-		});
-
+		containerElem.innerHTML += `
+<div class="card-front">
+	<p>${card.front}</p>
+</div>
+<div class="card-back none">
+	<p>${card.back}</p>
+</div>
+`;
 	});
+	containerElem.innerHTML += '</div>';
+	const frontCards = document.querySelectorAll('.card-front')
+	const backCards = document.querySelectorAll('.card-back')
+
+	frontCards.forEach(frontCard => {
+		frontCard.addEventListener('click', function (e) {
+			frontCard.classList.add('none')
+			backCards[currentCardIndex].classList.remove('none')
+		});
+	});
+
+	backCards.forEach(backCard => {
+		backCard.addEventListener('click', function (e) {
+			backCard.classList.add('none')
+			frontCards[currentCardIndex].classList.remove('none')
+		});
+	});
+
+	currentEl.innerHTML = `${currentCardIndex + 1}/${cards.length} `
 };
 
 
---
+++ test/test.js
@@ -0,0 +1,37 @@
+const containerElem = document.querySelector('.container');
+
+const flashcards = [
+	{
+		front: 'fff1',
+		back: 'bbb1'
+	},
+	{
+		front: 'fff2',
+		back: 'bbb2'
+	},
+	{
+		front: 'fff3',
+		back: 'bbb3'
+	}
+];
+
+containerElem.innerHTML = `
+${flashcards.map(flashcard => {
+	return `
+		<div class="flashcard">
+			<div class="front">${flashcard.front}</div>	
+			<div class="back">${flashcard.back}</div>	
+		</div>	
+	`;
+}).join('')}
+`;
+
+const flashcardFrontElems = document.querySelectorAll('.front');
+const flashcardBackElems = document.querySelectorAll('.back');
+flashcardFrontElems.forEach((m, index) => m.addEventListener('click', (e) => {
+	if (flashcardBackElems[index].style.display === 'none') {
+		flashcardBackElems[index].style.display = 'block';
+	} else {
+		flashcardBackElems[index].style.display = 'none';
+	}
+}));	
